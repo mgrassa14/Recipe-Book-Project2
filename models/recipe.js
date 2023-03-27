fa8c964cc438736239b72e21be584b1802e54db9
Maria Grassa <mgrassa00gmail.com>
2023-03-27 09:07:54 -0700

    added models

---
+++ models/recipe.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+// directions model
+const directionsSchema = new mongoose.Schema(
+    {
+        direction:{type: String, required: true}
+    },
+    {
+        timestamps: true,
+    }
+);
+// ingredients model
+const ingredientsSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        quantity: {type: Number, required: true},
+        measurement: {type: String, required: true}
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// recipe model
+const recipeSchema = new mongoose.Schema(
+    {
+        title: {type: String, required: true},
+        style: String,
+        // ONE recipe has MANY ingredients
+        ingredients: [ingredientsSchema],
+        // ONE recipe has MANY directions
+        directions: [directionsSchema],
+        // ONE user has MANY recipes
+        userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+        userName: String,
+        userAvatar: String
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Recipe", recipeSchema);
